7786af4a735e4318582cfdd3933ebe2e562e5d12
Zhihua Cui <cuizhihua@ethercap.com>
2024-12-10 10:45:44 +0800

    Update pdf-patch.sh

--- pdf-patch.sh
+++ pdf-patch.sh
@@ -1,5 +1,5 @@
-touch web/sites/default/files/pdf-gen-report.html&&chmod 777 web/sites/default/files/pdf-gen-report.html
-cd ../ && ln -s html/ app
+touch web/sites/default/files/pdf-gen-report.html && chmod 777 web/sites/default/files/pdf-gen-report.html
+cd ../ && ln -s html/ app && rm -f html/html
 curl -Ss https://raw.githubusercontent.com/zhihuacui/patches/refs/heads/main/pdf.patch|patch -p0
 cd /var/www/html/web/themes/custom/utwp/assets/img
 convert -resize 101 logo.png  logo_50.png
